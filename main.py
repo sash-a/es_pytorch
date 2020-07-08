253d67f21f7124681ba515aaee4c31543806b4dc
sasha <reallysasha@gmail.com>
2020-07-08 21:27:16 +0200

    Changed genome from requiring list of layers to requiring an nn.Module
    This allows more flexibility as any network architecture can be used

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ import pickle
 import json
 from collections import namedtuple
 
+import gym
+# noinspection PyUnresolvedReferences
+import pybullet_envs
+
+import torch
 import wandb
 import numpy as np
 from mpi4py import MPI
 
-from genome import Genome
+from nn_structures import FullyConnected
 from noisetable import NoiseTable
 from optimizers import Adam
 import gym_runner
+from policy import Policy
 
 
 def percent_rank(fits: np.ndarray):
@@ -41,28 +47,36 @@ if __name__ == '__main__':
     layer_sizes = [(15, 256), (256, 256), (256, 256), (256, 3)]
 
     noise = NoiseTable(comm, seed=cfg.seed, table_size=cfg.table_size)
-    geno = Genome(comm, layer_sizes, cfg.noise_stdev)
-    n_params = len(geno.params)
-    optim = Adam(geno.params, 0.01)
 
-    for i in range(cfg.gens):
+    policy = Policy(FullyConnected(15, 3, 256, 2, torch.nn.Tanh), cfg.noise_stdev)
+    optim = Adam(policy.flat_params, 0.01)
+    env = gym.make(cfg.env_name)
+
+    for gen in range(cfg.gens):
         results = []
         for _ in range(cfg.eps_per_gen):
-            results.append(gym_runner.run_genome(geno, noise, cfg.env_name, cfg.max_env_steps, cfg.eps_per_policy))
+            net, idx = policy.pheno(noise)
+            fitness = gym_runner.run_model(net, env, cfg.max_env_steps)
+            results += [(fitness, idx)]
 
         results = np.array(results * nproc)
         comm.Alltoall(results, results)
 
         if rank == 0:
             fits = results[:, 0]
-            wandb.log({'average': np.mean(fits), 'max': np.max(fits)})
-            print(f'\n\ngen:{i}\navg:{np.mean(fits)}\nmax:{np.max(fits)}\nfits:{fits}')
+            avg = np.mean(fits)
+            mx = np.max(fits)
+
+            wandb.log({'average': avg, 'max': mx})
+            print(f'\n\ngen:{gen}\navg:{avg}\nmax:{mx}')
 
         fits = percent_rank(results[:, 0])
         noise_inds = results[:, 1]
 
-        approx_grad = np.dot(fits, np.array([noise.get(int(idx), n_params) for idx in noise_inds])) / n_params
-        _, geno.params = optim.update(geno.params * cfg.l2coeff - approx_grad)
+        approx_grad = np.dot(fits, np.array([noise.get(int(i), policy.n_params) for i in noise_inds])) / policy.n_params
+        _, policy.flat_params = optim.update(policy.flat_params * cfg.l2coeff - approx_grad)
 
-        if rank == 0 and i % 1000 == 0:
-            pickle.dump(geno, open(f'saved/genome-{i}', 'wb'))
+        if rank == 0 and gen % 1000 == 0:
+            pickle.dump(policy, open(f'saved/genome-{gen}', 'wb'))
+
+    env.close()
