Fixed obstat overflow bug, simplified reporters and made run_saved.py work for both mujoco and bullet

es/evo/es.py
@@ -32,8 +32,7 @@ def step(cfg,
          fit_fn: Callable[[Module, gym.Env, int, Optional[RandomState]], TrainingResult],
          rs: RandomState = np.random.RandomState(),
          rank_fn: Callable[[Sequence[ndarray]], ndarray] = compute_centered_ranks,
-         obstat: Optional[ObStat] = None,
-         reporter: Reporter = StdoutReporter(MPI.COMM_WORLD)) -> TrainingResult:
+         reporter: Reporter = StdoutReporter(MPI.COMM_WORLD)) -> [TrainingResult, ObStat]:
     """
     Runs a single generation of ES
     :param fit_fn: Evaluates the policy returns a TrainingResult
@@ -44,6 +43,7 @@ def step(cfg,
     eps_per_proc = int((cfg.general.eps_per_gen / comm.size) / 2)
 
     gen_start = time.time()
+    gen_obstat = ObStat(env.observation_space.shape, 0)
     reporter.start_gen()
     results_pos, results_neg, inds = [], [], []
 
@@ -53,8 +53,8 @@ def step(cfg,
         # for each noise ind sampled, both add and subtract the noise
         results_pos.append(fit_fn(policy.pheno(noise), env, cfg.env.max_steps, rs))
         results_neg.append(fit_fn(policy.pheno(-noise), env, cfg.env.max_steps, rs))
-        obstat.inc(*results_pos[-1].ob_sum_sq_cnt)
-        obstat.inc(*results_neg[-1].ob_sum_sq_cnt)
+        gen_obstat.inc(*results_pos[-1].ob_sum_sq_cnt)
+        gen_obstat.inc(*results_neg[-1].ob_sum_sq_cnt)
 
     n_objectives = len(results_pos[0].result)
 
@@ -63,13 +63,13 @@ def step(cfg,
     ranked_fits = rank_fn(np.concatenate((fits_pos, fits_neg)))
     ranked_fits = ranked_fits[:len(fits_pos)] - ranked_fits[len(fits_pos):]
     noise_inds = results[:, -1]
-    obstat.mpi_inc(comm)
+    gen_obstat.mpi_inc(comm)
 
     _approx_grad(ranked_fits, noise_inds, nt, policy.flat_params, optim, cfg)
     noiseless_result = fit_fn(policy.pheno(np.zeros(len(policy))), env, cfg.env.max_steps, rs)
-    _report(reporter, np.concatenate((fits_pos, fits_neg), 0), policy, noiseless_result, gen_start)
+    reporter.end_gen(np.concatenate((fits_pos, fits_neg), 0), noiseless_result, policy, time.time() - gen_start)
 
-    return noiseless_result
+    return noiseless_result, gen_obstat
 
 
 def _share_results(comm: MPI.Comm,
@@ -89,9 +89,3 @@ def _approx_grad(ranked_fits: ndarray, inds: ndarray, nt: NoiseTable, flat_param
     """approximating gradient and update policy params"""
     grad = scale_noise(ranked_fits, inds, nt, cfg.general.batch_size) / cfg.general.eps_per_gen
     optim.step(cfg.general.l2coeff * flat_params - grad)
-
-
-def _report(rep: Reporter, fits: ndarray, policy: Policy, noiseless_result: TrainingResult, start: float):
-    rep.report_noiseless(noiseless_result, policy)
-    rep.report_fits(fits)
-    rep.end_gen(time.time() - start)

es/utils/reporters.py
@@ -20,16 +20,7 @@ class Reporter(ABC):
         pass
 
     @abstractmethod
-    def report_fits(self, fits: np.ndarray):
-        pass
-
-    @abstractmethod
-    def report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
-        """Reports the fitness of a evaluation using no noise from the table and noiseless actions"""
-        pass
-
-    @abstractmethod
-    def end_gen(self, time: float):
+    def end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         pass
 
 
@@ -37,21 +28,13 @@ class ReporterSet(Reporter):
     def __init__(self, *reporters: Reporter):
         self.reporters = reporters
 
-    def report_fits(self, fits: np.ndarray):
-        for reporter in self.reporters:
-            reporter.report_fits(fits)
-
     def start_gen(self):
         for reporter in self.reporters:
             reporter.start_gen()
 
-    def report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
+    def end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         for reporter in self.reporters:
-            reporter.report_noiseless(tr, noiseless_policy)
-
-    def end_gen(self, time: float):
-        for reporter in self.reporters:
-            reporter.end_gen(time)
+            reporter.end_gen(fits, noiseless_tr, noiseless_policy, time)
 
 
 class MPIReporter(Reporter, ABC):
@@ -62,34 +45,16 @@ class MPIReporter(Reporter, ABC):
         if self.comm.rank == 0:
             self._start_gen()
 
-    def report_fits(self, fits: np.ndarray):
+    def end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         if self.comm.rank == 0:
-            self._report_fits(fits)
-
-    def report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
-        """Reports the fitness of a evaluation using no noise from the table and noiseless actions"""
-        if self.comm.rank == 0:
-            self._report_noiseless(tr, noiseless_policy)
-
-    def end_gen(self, time: float):
-        if self.comm.rank == 0:
-            self._end_gen(time)
+            self._end_gen(fits, noiseless_tr, noiseless_policy, time)
 
     @abstractmethod
     def _start_gen(self):
         pass
 
     @abstractmethod
-    def _report_fits(self, fits: np.ndarray):
-        pass
-
-    @abstractmethod
-    def _report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
-        """Reports the fitness of a evaluation using no noise from the table and noiseless actions"""
-        pass
-
-    @abstractmethod
-    def _end_gen(self, time: float):
+    def _end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         pass
 
 
@@ -104,22 +69,20 @@ class StdoutReporter(MPIReporter):
               f'----------------------------------------'
               f'\ngen:{self.gen}')
 
-    def _report_fits(self, fits: np.ndarray):
+    def _end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         for i, col in enumerate(fits.T):
             # Objectives are grouped by column so this finds the avg and max of each objective
             print(f'obj {i} avg:{np.mean(col):0.2f}')
             print(f'obj {i} max:{np.max(col):0.2f}')
 
-    def _report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
-        print(f'fit:{tr.result}')
+        print(f'fit:{noiseless_tr.result}')
         # Calculating distance traveled (ignoring height dim). Assumes starting at 0, 0
-        dist = np.linalg.norm(np.array(tr.behaviour[-3:-1]))
-        rew = np.sum(tr.rewards)
+        dist = np.linalg.norm(np.array(noiseless_tr.behaviour[-3:-1]))
+        rew = np.sum(noiseless_tr.rewards)
 
         print(f'dist:{dist}')
         print(f'rew:{rew}')
 
-    def _end_gen(self, time: float):
         print(f'time:{time:0.2f}')
         self.gen += 1
 
@@ -143,27 +106,20 @@ class LoggerReporter(MPIReporter):
     def _start_gen(self):
         logging.info(f'gen:{self.gen}')
 
-    def _report_fits(self, fits: np.ndarray):
+    def _end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         for i, col in enumerate(fits.T):
             # Objectives are grouped by column so this finds the avg and max of each objective
             logging.info(f'obj {i} avg:{np.mean(col):0.2f}')
             logging.info(f'obj {i} max:{np.max(col):0.2f}')
 
-    def _report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
-        logging.info(f'fit:{tr.result}')
+        logging.info(f'fit:{noiseless_tr.result}')
         # Calculating distance traveled (ignoring height dim). Assumes starting at 0, 0
-        dist = np.linalg.norm(np.array(tr.behaviour[-3:-1]))
-        rew = np.sum(tr.rewards)
+        dist = np.linalg.norm(np.array(noiseless_tr.behaviour[-3:-1]))
+        rew = np.sum(noiseless_tr.rewards)
 
         logging.info(f'dist:{dist}')
         logging.info(f'rew:{rew}')
 
-        if rew > self.best_rew or dist > self.best_dist:
-            self.best_rew = max(rew, self.best_rew)
-            self.best_dist = max(dist, self.best_dist)
-            noiseless_policy.save(f'saved/{self.cfg.general.name}', str(self.gen))
-
-    def _end_gen(self, time: float):
         logging.info(f'time:{time:0.2f}')
         self.gen += 1
 
@@ -184,19 +140,17 @@ class MLFlowReporter(MPIReporter):
     def _start_gen(self):
         pass
 
-    def _report_fits(self, fits: np.ndarray):
+    def _end_gen(self, fits: np.ndarray, noiseless_tr: TrainingResult, noiseless_policy: Policy, time: float):
         for i, col in enumerate(fits.T):
             # Objectives are grouped by column so this finds the avg and max of each objective
             log_metric(f'obj {i} avg', np.mean(col), self.gen)
             log_metric(f'obj {i} max', np.max(col), self.gen)
 
-    def _report_noiseless(self, tr: TrainingResult, noiseless_policy: Policy):
         # Calculating distance traveled (ignoring height dim). Assumes starting at 0, 0
-        dist = np.linalg.norm(np.array(tr.behaviour[-3:-1]))
-        rew = np.sum(tr.rewards)
+        dist = np.linalg.norm(np.array(noiseless_tr.behaviour[-3:-1]))
+        rew = np.sum(noiseless_tr.rewards)
 
         log_metric('dist', dist, self.gen)
         log_metric('rew', rew, self.gen)
 
-    def _end_gen(self, time: float):
         self.gen += 1

obj.py
@@ -19,6 +19,7 @@ from es.utils.utils import moo_mean_rank, compute_centered_ranks
 
 if __name__ == '__main__':
     comm: MPI.Comm = MPI.COMM_WORLD
+    gym.logger.set_level(40)
 
     cfg_file = utils.parse_args()
     cfg = utils.load_config(cfg_file)
@@ -49,6 +50,9 @@ if __name__ == '__main__':
 
     rank_fn = partial(moo_mean_rank, rank_fn=compute_centered_ranks)
 
+    best_rew = -np.inf
+    best_dist = -np.inf
+
 
     def r_fn(model: torch.nn.Module, e: gym.Env, max_steps: int, r: np.random.RandomState = None) -> TrainingResult:
         save_obs = (r.random() if r is not None else np.random.random()) < cfg.policy.save_obs_chance
@@ -58,6 +62,14 @@ if __name__ == '__main__':
 
     for gen in range(cfg.general.gens):
         nn.set_ob_mean_std(obstat.mean, obstat.std)
-        tr = es.step(cfg, comm, policy, optim, nt, env, r_fn, rs, rank_fn, obstat, reporter)
+        tr, gen_obstat = es.step(cfg, comm, policy, optim, nt, env, r_fn, rs, rank_fn, reporter)
+        obstat += gen_obstat  # adding the new observations to the global obstat
+
+        # Saving policy if it obtained a better reward or distance
+        dist = np.linalg.norm(np.array(tr.behaviour[-3:-1]))
+        if tr.rew > best_rew or dist > best_dist:
+            best_rew = max(tr.rew, best_rew)
+            best_dist = max(dist, best_dist)
+            policy.save(f'saved/{cfg.general.name}', str(gen))
 
     mlflow.end_run()  # in the case where mlflow is the reporter, just ending its run

run_saved.py
@@ -4,17 +4,19 @@ import gym
 import numpy as np
 
 from es.evo.policy import Policy
-from es.utils.gym_runner import run_model
+from es.utils.gym_runner import run_model, BULLET_ENV_SUFFIX
 
 
 def run_saved(policy_path: str, env: gym.Env, steps: int):
     p = Policy.load(policy_path)
     while True:
-        r, d, _= run_model(p.pheno(np.zeros(len(p))), env, steps, render=True)
+        r, d, _ = run_model(p.pheno(np.zeros(len(p))), env, steps, render=True)
         print(f'\n\nrewards {np.sum(r)}\ndist {np.linalg.norm(np.array(d[-3:-1]))}\n\n')
 
 
 if __name__ == '__main__':
+    gym.logger.set_level(40)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('env')
     parser.add_argument('pickle_file')
@@ -23,6 +25,10 @@ if __name__ == '__main__':
     # noinspection PyUnresolvedReferences
     import pybullet_envs
 
-    e = gym.make(args.env)
+    if BULLET_ENV_SUFFIX in args.env:
+        e = gym.make(args.env, render=True)
+    else:
+        e = gym.make(args.env)
+
     run_saved(args.pickle_file, e, 10000)
     e.close()