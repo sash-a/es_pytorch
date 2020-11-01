99ad2797f4063b4622d085f8b2f4ca9e84efe9fd
sasha <reallysasha@gmail.com>
2020-11-01 17:55:25 +0200

    Implemented a reset to best ever params

--- obj.py
+++ obj.py
@@ -57,6 +57,7 @@ if __name__ == '__main__':
     best_rew = -np.inf
     best_dist = -np.inf
     best_max_rew = -np.inf  # highest achieved in any gen
+    best_params = policy.flat_params
 
 
     def r_fn(model: torch.nn.Module) -> TrainingResult:
@@ -65,8 +66,11 @@ if __name__ == '__main__':
         return RewardResult(rews, behv, obs, steps)
 
 
-    time_since_best = 0
+    time_since_max_best = 0
+    time_since_noiseless_best = 0
     noise_std_inc = 0.08
+    eps_per_proc = int((cfg.general.policies_per_gen / comm.size) / 2)
+
     for gen in range(cfg.general.gens):
         mlflow_reporter.set_active_run(0)
         reporter.start_gen()
@@ -76,9 +80,22 @@ if __name__ == '__main__':
         if cfg.policy.lr_decay != 1:
             reporter.log({'lr': cfg.policy.lr})
 
+        # the step
         nn.set_ob_mean_std(obstat.mean, obstat.std)
-        tr, gen_obstat = es.step(cfg, comm, policy, optim, nt, env, r_fn, rs, ranker, reporter)
-        obstat += gen_obstat  # adding the new observations to the global obstat
+        gen_obstat = ObStat(env.observation_space.shape, 0)
+        pos_fits, neg_fits, inds, steps = es.test_params(comm, eps_per_proc, policy, nt, gen_obstat, r_fn, rs)
+        obstat += gen_obstat
+        ranker.rank(pos_fits, neg_fits, inds)
+
+        # saving the best ever params
+        max_ind = np.argmax(ranker.fits[:, 0]) % len(inds)
+        max_rew = ranker.fits[:, 0][max_ind]
+        if max_rew > best_max_rew:
+            best_params = policy.flat_params + nt[int(inds[max_ind])] * policy.std
+
+        es.approx_grad(ranker, nt, policy.flat_params, optim, cfg.general.batch_size, cfg.policy.l2coeff)
+
+        tr = r_fn(policy.pheno(np.zeros(len(policy))))  # noiseless result after param update
 
         cfg.noise.std = policy.std = max(cfg.noise.std * cfg.noise.std_decay, cfg.noise.std_limit)
         cfg.policy.lr = optim.lr = max(cfg.policy.lr * cfg.policy.lr_decay, cfg.policy.lr_limit)
@@ -87,25 +104,34 @@ if __name__ == '__main__':
 
         dist = np.linalg.norm(np.array(tr.positions[-3:-1]))
         rew = np.sum(tr.rewards)
-        max_rew = np.max(ranker.fits[:, 0])
 
-        time_since_best = 0 if max_rew > best_max_rew else time_since_best + 1
-        reporter.log({'time since best': time_since_best})
-        # increasing noise std if policy is stuck
-        if time_since_best > cfg.experimental.max_time_since_best and cfg.experimental.explore_with_large_noise:
-            cfg.noise.std = policy.std = policy.std + noise_std_inc
+        time_since_max_best = 0 if max_rew > best_max_rew else time_since_max_best + 1
+        time_since_noiseless_best = 0 if rew > best_rew else time_since_noiseless_best + 1
+        reporter.log({'time since max best': time_since_max_best})
+        reporter.log({'time since noiseless best': time_since_noiseless_best})
 
-        if 0 < cfg.experimental.elite < 1:  # using elite extension
-            if time_since_best > cfg.experimental.max_time_since_best and cfg.experimental.elite < 1:
+        # Experimental ways to deal with policy being stuck
+        if time_since_max_best > cfg.experimental.max_time_since_best:
+            if cfg.experimental.explore_with_large_noise:  # increasing noise std if policy is stuck
+                cfg.noise.std = policy.std = policy.std + noise_std_inc
+
+            if cfg.experimental.elite < 1:  # elite extension
                 ranker.elite_percent = cfg.experimental.elite
-            if time_since_best == 0:
+
+            if cfg.experimental.reset_to_best:
+                policy.flat_params = best_params
+
+        if time_since_max_best == 0:
+            if cfg.experimental.explore_with_large_noise:
+                cfg.noise.std = policy.std = policy.std - noise_std_inc
+
+            if cfg.experimental.elite < 1:
                 ranker.elite_percent = 1
-            reporter.print(f'elite percent: {ranker.elite_percent}')
 
         save_policy = (rew > best_rew or dist > best_dist)
         best_rew = max(rew, best_rew)
         best_dist = max(dist, best_dist)
-        best_max_rew = (best_max_rew, max_rew)
+        best_max_rew = max(best_max_rew, max_rew)
 
         # Saving policy if it obtained a better reward or distance
         if save_policy and comm.rank == 0:
