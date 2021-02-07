5b2c72db991c267f5670e076afa858872336b94a
sash-a <reallysasha@gmail.com>
2021-02-07 12:15:35 +0200

    Updated reporters

--- obj.py
+++ obj.py
@@ -18,21 +18,21 @@ from src.nn.obstat import ObStat
 from src.nn.optimizers import Adam, Optimizer
 from src.utils import utils
 from src.utils.rankers import CenteredRanker, EliteRanker
-from src.utils.reporters import LoggerReporter, ReporterSet, StdoutReporter, MLFlowReporter
+from src.utils.reporters import LoggerReporter, StdoutReporter, MLFlowReporter, DefaultMpiReporterSet
 
 
 def main(cfg):
     comm: MPI.Comm = MPI.COMM_WORLD
 
-    full_name = f'{os.path.split(cfg.env.name)[1][:-7]}-{cfg.general.name}' \
-        if os.path.exists(cfg.env.name) else f'{cfg.env.name}-{cfg.general.name}'
+    full_name = f'{cfg.env.name}-{cfg.general.name}'
 
     mlflow_reporter = MLFlowReporter(comm, cfg) if cfg.general.mlflow else None
-    reporter = ReporterSet(
-        LoggerReporter(comm, full_name),
-        StdoutReporter(comm),
-        mlflow_reporter
-    )
+    if cfg.general.mlflow: mlflow_reporter.set_active_run(0)
+
+    reporter = DefaultMpiReporterSet(comm, full_name,
+                                     LoggerReporter(comm, full_name),
+                                     StdoutReporter(comm),
+                                     mlflow_reporter)
 
     env: gym.Env = UnityGymWrapper(cfg.env.name, comm.rank) if os.path.exists(cfg.env.name) else gym.make(cfg.env.name)
 
@@ -58,8 +58,6 @@ def main(cfg):
     if 0 < cfg.experimental.elite < 1:
         ranker = EliteRanker(CenteredRanker(), cfg.experimental.elite)
 
-    best_rew = -np.inf
-    best_dist = -np.inf
     best_max_rew = -np.inf  # highest achieved in any gen
 
     def r_fn(model: torch.nn.Module, use_ac_noise=True) -> TrainingResult:
@@ -75,8 +73,8 @@ def main(cfg):
 
     time_since_best = 0
     noise_std_inc = 0.08
+
     for gen in range(cfg.general.gens):
-        if cfg.general.mlflow: mlflow_reporter.set_active_run(0)
         reporter.start_gen()
 
         if cfg.noise.std_decay != 1:
@@ -96,8 +94,6 @@ def main(cfg):
 
         reporter.log({'obs recorded': obstat.count})
 
-        dist = np.linalg.norm(np.array(tr.positions[-3:-1]))
-        rew = np.sum(tr.rewards)
         max_rew_ind = np.argmax(ranker.fits[:, 0])
         max_rew = ranker.fits[:, 0][max_rew_ind]
 
@@ -113,15 +109,6 @@ def main(cfg):
             if time_since_best == 0:
                 ranker.elite_percent = 1
             reporter.print(f'elite percent: {ranker.elite_percent}')
-
-        save_policy = (rew > best_rew or dist > best_dist)
-        best_rew = max(rew, best_rew)
-        best_dist = max(dist, best_dist)
-
-        # Saving policy if it obtained a better reward or distance
-        if save_policy and comm.rank == 0:
-            policy.save(path.join('saved', full_name, 'weights'), str(gen))
-            reporter.print(f'saving policy with rew:{rew:0.2f} and dist:{dist:0.2f}')
 
         # Saving max rew if it obtained best ever rew
         if max_rew > best_max_rew and comm.rank == 0:
--- unity.py
+++ unity.py
@@ -1,6 +1,7 @@
 import os
 
 import gym
+import mlflow
 import numpy as np
 import torch
 from mpi4py import MPI
@@ -16,6 +17,7 @@ from src.nn.obstat import ObStat
 from src.nn.optimizers import Adam, Optimizer
 from src.utils import utils
 from src.utils.rankers import CenteredRanker
+from src.utils.reporters import MLFlowReporter, LoggerReporter, StdoutReporter, DefaultMpiReporterSet
 from src.utils.utils import generate_seed
 
 if __name__ == '__main__':
@@ -27,14 +29,21 @@ if __name__ == '__main__':
     full_name = f'{os.path.split(cfg.env.name)[1][:-7]}-{cfg.general.name}'
     env: gym.Env = UnityGymWrapper(cfg.env.name, comm.rank)
 
+    mlflow_reporter = MLFlowReporter(comm, cfg) if cfg.general.mlflow else None
+    if cfg.general.mlflow: mlflow_reporter.set_active_run(0)
+    reporter = DefaultMpiReporterSet(comm, full_name,
+                                     LoggerReporter(comm, full_name),
+                                     StdoutReporter(comm),
+                                     mlflow_reporter)
+
     # seeding; this must be done before creating the neural network so that params are deterministic across processes
     cfg.general.seed = (generate_seed(comm) if cfg.general.seed is None else cfg.general.seed)
     rs = utils.seed(comm, cfg.general.seed, env)
 
     # initializing obstat, policy, optimizer, noise and ranker
     obstat: ObStat = ObStat(env.observation_space.shape, 1e-2)  # eps to prevent dividing by zero at the beginning
-    nn = FullyConnected(int(np.prod(env.observation_space.shape)), int(np.prod(env.action_space.shape)),
-                        256, 2, torch.nn.Tanh(), env, cfg.policy)
+    nn = FullyConnected(int(np.prod(env.observation_space.shape)), int(np.prod(env.action_space.shape)), 256, 2,
+                        torch.nn.Tanh(), env, cfg.policy)
     policy: Policy = Policy(nn, cfg.noise.std)
     optim: Optimizer = Adam(policy, cfg.policy.lr)
     nt: NoiseTable = NoiseTable.create_shared(comm, cfg.noise.tbl_size, len(policy), None, cfg.general.seed)
@@ -50,18 +59,16 @@ if __name__ == '__main__':
     assert cfg.general.policies_per_gen % comm.size == 0 and (cfg.general.policies_per_gen / comm.size) % 2 == 0
     eps_per_proc = int((cfg.general.policies_per_gen / comm.size) / 2)
     for gen in range(cfg.general.gens):  # main loop
-        if comm.rank == 0: print(f'Generation:{gen}')  # only print on one process
+        reporter.start_gen()
         nn.set_ob_mean_std(obstat.mean, obstat.std)  # for normalizing the observation space
 
-        # the block below is encapsulated in es.step(...), but this is more flexible. Example use can be seen in obj.py
         gen_obstat = ObStat(env.observation_space.shape, 0)  # for normalizing the observation space
-        # obtaining the fitnesses from many perturbed policies
         pos_fits, neg_fits, inds, steps = es.test_params(comm, eps_per_proc, policy, nt, gen_obstat, r_fn, rs)
         obstat += gen_obstat  # adding the new observations to the global obstat
-        ranker.rank(pos_fits, neg_fits, inds)  # ranking the fitnesses between -1 and 1
-        # approximating the gradient and updating policy.flat_params (pseudo backprop)
+        ranker.rank(pos_fits, neg_fits, inds)
         es.approx_grad(ranker, nt, policy.flat_params, optim, cfg.general.batch_size, cfg.policy.l2coeff)
 
-        if comm.rank == 0: print(f'avg fitness:{np.mean(np.concatenate((pos_fits, neg_fits)))}\n\n')
-        if gen % 10 and comm.rank == 0:  # save policy every 10 generations
-            policy.save(f'saved/{cfg.general.name}', str(gen))
+        reporter.log_gen(ranker.fits, r_fn(policy.pheno()), policy, steps)
+        reporter.end_gen()
+
+    mlflow.end_run()
