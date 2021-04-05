807fcf7856d167a7532a67633b1c7f0f7e57f54c
sash-a <reallysasha@gmail.com>
2021-04-05 19:05:05 +0200

    Updated run_saved.py to work with rand_pos

--- run_saved.py
+++ run_saved.py
@@ -4,21 +4,30 @@ import gym
 import numpy as np
 import torch
 
+import rand_pos as rp
+# noinspection PyUnresolvedReferences
+from rand_pos import PrimFF
 from src.core.policy import Policy
 from src.gym.gym_runner import run_model
 
 
-def run_saved_policy(policy_path: str, env: gym.Env, steps: int):
-    run_saved(Policy.load(policy_path).pheno(), env, steps)
+def run_saved_policy(policy_path: str, env: gym.Env, steps: int, rand_pos):
+    run_saved(Policy.load(policy_path).pheno(), env, steps, rand_pos)
 
 
-def run_saved_pytorch(policy_path: str, env: gym.Env, steps: int):
-    run_saved(torch.load(policy_path), env, steps)
+def run_saved_pytorch(policy_path: str, env: gym.Env, steps: int, rand_pos):
+    run_saved(torch.load(policy_path), env, steps, rand_pos)
 
 
-def run_saved(model: torch.nn.Module, env, steps):
+def run_saved(model: torch.nn.Module, env, steps, rand_pos):
     while True:
-        r, d, _, s = run_model(model, env, steps, render=True)
+        if rand_pos:
+            goal = torch.tensor(rp.gen_goal(np.random.RandomState()))
+            print(f'goal:{goal}')
+            r, d, _, s = rp.run_model(model, env, steps, render=True, goal_normed=goal)
+        else:
+            r, d, _, s = run_model(model, env, steps, render=True)
+
         print(f'\n\nrewards {np.sum(r)}\ndist {np.linalg.norm(np.array(d[-3:-1]))}\n\n')
 
 
@@ -28,6 +37,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('env')
     parser.add_argument('pickle_file')
+    parser.add_argument('--rand_pos', dest='rand_pos', action='store_true')
+    parser.add_argument('--no-rand_pos', dest='rand_pos', action='store_false')
+    parser.set_defaults(rand_pos=True)
+
     args = parser.parse_args()
 
     # # noinspection PyUnresolvedReferences
@@ -35,9 +48,10 @@ if __name__ == '__main__':
 
     e = gym.make(args.env)
     e.render('human')
+    steps = 500
 
     if args.pickle_file.endswith('.pt'):
-        run_saved_pytorch(args.pickle_file, e, 2000)
+        run_saved_pytorch(args.pickle_file, e, steps, args.rand_pos)
     else:
-        run_saved_policy(args.pickle_file, e, 2000)
+        run_saved_policy(args.pickle_file, e, steps, args.rand_pos)
     e.close()
