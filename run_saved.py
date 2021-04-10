ad4443687fc0ad18d21ba39adf6d17f20ff93530
sash-a <reallysasha@gmail.com>
2021-04-10 17:04:12 +0200

    Fixing run saved for new env

--- run_saved.py
+++ run_saved.py
@@ -4,29 +4,23 @@ import gym
 import numpy as np
 import torch
 
-import flagrun as rp
+import flagrun
 # noinspection PyUnresolvedReferences
 from flagrun import PrimFF
 from src.core.policy import Policy
-from src.gym.gym_runner import run_model
 
 
-def run_saved_policy(policy_path: str, env: gym.Env, steps: int, rand_pos):
-    run_saved(Policy.load(policy_path).pheno(), env, steps, rand_pos)
+def run_saved_policy(policy_path: str, env: gym.Env, steps: int):
+    run_saved(Policy.load(policy_path).pheno(), env, steps)
 
 
-def run_saved_pytorch(policy_path: str, env: gym.Env, steps: int, rand_pos):
-    run_saved(torch.load(policy_path), env, steps, rand_pos)
+def run_saved_pytorch(policy_path: str, env: gym.Env, steps: int):
+    run_saved(torch.load(policy_path), env, steps)
 
 
-def run_saved(model: torch.nn.Module, env, steps, rand_pos):
+def run_saved(model: torch.nn.Module, env, steps):
     while True:
-        if rand_pos:
-            goal = torch.tensor(rp.gen_goal(np.random.RandomState(), -1))
-            print(f'goal:{goal}')
-            r, d, _, s = rp.run_model(model, env, steps, render=True, goal_normed=goal)
-        else:
-            r, d, _, s = run_model(model, env, steps, render=True)
+        r, d, _, s = flagrun.run_model(model, env, steps, render=True)
 
         print(f'\n\nrewards {np.sum(r)}\ndist {np.linalg.norm(np.array(d[-3:-1]))}\n\n')
 
@@ -37,8 +31,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('env')
     parser.add_argument('pickle_file')
-    parser.add_argument('--rand_pos', dest='rand_pos', action='store_true')
-    parser.set_defaults(rand_pos=False)
     parser.add_argument('--record', dest='record', action='store_true')
     parser.set_defaults(record=False)
 
@@ -48,6 +40,7 @@ if __name__ == '__main__':
     # import pybullet_envs
 
     e = gym.make(args.env).unwrapped
+
     e.render('human')
     e.reset()
     steps = 2000
@@ -55,7 +48,7 @@ if __name__ == '__main__':
         e.scene._p.startStateLogging(e.scene._p.STATE_LOGGING_VIDEO_MP4, '~/Documents/es/testvid.mp4')
 
     if args.pickle_file.endswith('.pt'):
-        run_saved_pytorch(args.pickle_file, e, steps, args.rand_pos)
+        run_saved_pytorch(args.pickle_file, e, steps)
     else:
-        run_saved_policy(args.pickle_file, e, steps, args.rand_pos)
+        run_saved_policy(args.pickle_file, e, steps)
     e.close()
